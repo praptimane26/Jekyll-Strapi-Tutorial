80dbe30fc700454605d3cb3622b9bcfb6a3ad71a
praptimane26 <praptimane26@github.com>
2021-10-18 11:36:52 +1300

    commit all changes

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '11928589479a43f864929b6223cc74c9'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c9b92a92-a1ac-4040-b949-70e3cd63df21'
+};
